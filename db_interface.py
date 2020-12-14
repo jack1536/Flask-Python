e780d6cb6f768b119f45ab0dd0d2dc184f4d33b8
Jack Weber <jackdavidweber@gmail.com>
2020-12-14 17:11:12 -0500

    cleaned up db_interface

--- db_interface.py
+++ db_interface.py
@@ -6,13 +6,11 @@ import os
 from MySQLdb.constants import FIELD_TYPE
 
 
-
 host = '162.241.230.118'
 user = os.environ['MYSQL_USER']
 password = os.environ['MYSQL_PASSWORD']
 port = 3306
 db = 'codetran_collegedata'
-
 my_conv = { FIELD_TYPE.LONG: int, FIELD_TYPE.DECIMAL: int}
 
 conn = MySQLdb.Connection(
@@ -24,48 +22,12 @@ conn = MySQLdb.Connection(
     db=db
 )
 
-
-
-# conn=_mysql.connect(
-#     conv = my_conv,
-#     host=host,
-#     user=user,
-#     passwd=password,
-#     port=port,
-#     db=db
-# )
-
-
-def test_select():
-    print("test select started")
-
-    # Example of how to fetch table data:
-    conn.query("""SELECT * FROM james_table""")
-    result = conn.store_result()
-
-    l  = []
-    for i in range(result.num_rows()):
-        l.append(result.fetch_row())
-
-    print("returned list", l)
-    return l
-
-
 def execute_query(q):
     conn.query(q)
     result = conn.store_result()
 
-    # l  = []
-    # for i in range(result.num_rows()):
-    #     l.append(result.fetch_row())
-
-    # l = result.fetch_row(maxrows=0, how=1)
-
-    # print("returned list", l)
     return result
 
-
-# {'school_name': 'Columbia University in the City of New York', 'latest_student_size': Decimal('8216.0')}
 
 def query_to_json(q):
     result = execute_query(q)
@@ -88,16 +50,3 @@ def query_to_json(q):
     return out
 
 
-
-
-
-
-
-
-
-
-# execute_query("""SELECT * FROM james_table""")
-# execute_query("SELECT * FROM james_table")
-# execute_query("SELECT school_name, latest_student_size FROM codetran_collegedata.collegescorecard WHERE latest_admissions_act_scores_midpoint_cumulative BETWEEN 32 AND 36 AND latest_student_size BETWEEN 0 AND 50000")
-query_to_json("SELECT school_name, latest_student_size FROM codetran_collegedata.collegescorecard WHERE latest_admissions_act_scores_midpoint_cumulative BETWEEN 34 AND 30 AND latest_student_size BETWEEN 0 AND 50000")
-# test_select()
